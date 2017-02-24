f3a9ad4264abbffe91bd143dc58b193c13d8b098
zhangyh <1328729160@qq.com>
2017-02-24 15:32:41 +0800

    tainjia  view  absolute

---
+++ src/testzyhgit/随手想法.java
@@ -0,0 +1,8 @@
+
+源码  volley  uil eventbus  1 drawView  2 startactivity 3 bindercomit ,  asynctask  , handler , xmpp 
+回家看看有哪些源码, 百度那些源码问的多
+那些需要整理  直接看文档就可  整理源码网址 简单总结源码结构(源码深度,关键点),,针对一些公司的要求进行差缺 
+严格按照市时间规划 ,找几份jianli 参考下知识点 ,网上最新的面试经验, 
+
+
+224 view绘制原理  过程  自定义控件
